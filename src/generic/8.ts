0b0144cdd767a9786a63e2f13e84053e476297de
SuperNiceCat <131773661+SuperN1ceC6t@users.noreply.github.com>
2024-02-27 21:53:35 +0200

    First Commit

--- src/generic/8.ts
+++ src/generic/8.ts
@@ -19,6 +19,8 @@ type Form = {
 };
 
 // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+type Params = Omit<Form, "errors">;
 
 export {};
